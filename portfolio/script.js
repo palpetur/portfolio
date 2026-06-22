87a759eb980dc0b183355dc8ec2b3f04cd551a31
palpetur <pal.petur.donat@simonyiszki.org>
2026-06-22 13:54:07 +0200

    moreCSS

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,6 +1,7 @@
 function cssChange() {
 
     const value = document.querySelector(`input[name='css']:checked`).value;
-    document.getElementById("style").href = "css/style{value}.css";
-    console.log(value);
+    document.getElementById("style").href = `css/style${value}.css`;
+    console.log("css/style{value}.css");
+
 }
